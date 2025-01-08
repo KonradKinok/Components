58dbf8150d45958165f51024da0d47ee90e917dc
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2025-01-08 19:42:30 +0100

    08.01.2024

--- src/pages/FormsExamplePage/FormsExamplePage.tsx
+++ src/pages/FormsExamplePage/FormsExamplePage.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import scss from "./FormsExamplePage.module.scss";
 import { ContactForm } from "./components/ContactForm/ContactForm";
+import { ContactFormUpdate } from "./components/ContactFormUpdate/ContactFormUpdate";
 export const FormsExamplePage = () => {
   return (
     <div className={scss["FormsExamplePage-container"]}>
       <div className={scss["FormsExamplePage-contactForm-container"]}>
         <ContactForm />
+      </div>
+      <div className={scss["FormsExamplePage-contactForm-container"]}>
+        <ContactFormUpdate />
       </div>
     </div>
   );
--- src/pages/FormsExamplePage/components/ContactFormUpdate/ContactFormUpdate.tsx
+++ src/pages/FormsExamplePage/components/ContactFormUpdate/ContactFormUpdate.tsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from "react";
 import { FaUser, FaPhoneAlt } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import { SingleInput } from "../../../FormsPage/SingleInput/SingleInput";
-import scss from "./ContactForm.module.scss";
+import scss from "./ContactFormUpdate.module.scss";
 
-export const ContactForm = () => {
-  const [inputName, setInputName] = useState<string>("Konrad");
+export const ContactFormUpdate = () => {
+  const [inputId, setInputId] = useState<string>("BeWfJy-GwNiwFD6lZRKlY");
+  const [inputIdError, setInputIdError] = useState<string>("");
+  const [inputName, setInputName] = useState<string>("Marek");
   const [inputNameError, setInputNameError] = useState<string>("");
-  const [inputEmail, setInputEmail] = useState<string>("konrad@gmail.com");
+  const [inputEmail, setInputEmail] = useState<string>("Marek@gmail.com");
   const [inputEmailError, setInputEmailError] = useState<string>("");
-  const [inputPhone, setInputPhone] = useState<string>("989768567");
+  const [inputPhone, setInputPhone] = useState<string>("11111111");
   const [inputPhoneError, setInputPhoneError] = useState<string>("");
   const handleSingleInputChange = (
     event: React.ChangeEvent<HTMLInputElement>,
@@ -17,7 +19,15 @@ export const ContactForm = () => {
     const currentValue = event.target.value;
     const currentName = event.target.name;
     let errorTextInput = "";
-
+    if (currentName === "id") {
+      setInputId(currentValue);
+      if (currentValue.length === 1) {
+        errorTextInput = "Za mało liter";
+      } else if (!currentValue) {
+        errorTextInput = "Musisz wypełnić te pole";
+      }
+      setInputIdError(errorTextInput);
+    }
     if (currentName === "name") {
       setInputName(currentValue);
       if (currentValue.length === 1) {
@@ -50,21 +60,24 @@ export const ContactForm = () => {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:3000/api/contacts/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
+      const response = await fetch(
+        `http://localhost:3000/api/contacts/${inputId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: inputName,
+            email: inputEmail,
+            phone: inputPhone,
+          }),
         },
-        body: JSON.stringify({
-          name: inputName,
-          email: inputEmail,
-          phone: inputPhone,
-        }),
-      });
+      );
 
       if (response.ok) {
         const result = await response.json();
-        console.log("Contact added:", result);
+        console.log("Contact updated:", result);
       } else {
         const errorData = await response.json(); // Pobranie treści odpowiedzi z błędem
         console.error(errorData.message); // Wyświetlenie szczegółowego komunikatu błędu
@@ -75,7 +88,17 @@ export const ContactForm = () => {
   };
   return (
     <form className={scss["contact-form"]} onSubmit={handleSubmit}>
-      <h3 className={scss["contact-form-title"]}>Contact Form</h3>
+      <h3 className={scss["contact-form-title"]}>Contact Form - update</h3>
+      <SingleInput
+        inputName="id"
+        singleInputValue={inputId}
+        handleSingleInputChange={handleSingleInputChange}
+        inputPlaceholder="Enter contact Id"
+        iconLeft={<FaUser size={16} />}
+        singleInputError={inputIdError}
+        required={false}
+        classNameInputContainer={scss["custom-input-container"]}
+      />
       <SingleInput
         inputName="name"
         singleInputValue={inputName}
