f1cbe7e6fd456b726ecfa41adcbf1a985f5139a3
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2025-03-31 23:14:28 +0200

    31.03.2025

--- src/pages/CheckBoxPage/CheckBoxPage.tsx
+++ src/pages/CheckBoxPage/CheckBoxPage.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import scss from "./CheckBoxPage.module.scss";
 
 export default function CheckBoxPage() {
+  const [selectedOption, setSelectedOption] = useState("free");
+
   return (
     <div className={scss["container-checkbox-page"]}>
       <h1>ToolTip page</h1>
@@ -38,6 +40,40 @@ export default function CheckBoxPage() {
             className={scss["toggle-switch"]}
           />
         </form>
+      </div>
+      <div className={scss["radio-container-pionowy"]}>
+        <div className={scss["radio-container"]}>
+          <input
+            checked={selectedOption === "free"}
+            id="radio-free"
+            name="radio"
+            type="radio"
+            onChange={() => setSelectedOption("free")}
+          />
+          <label htmlFor="radio-free">Data otrzymania faktury</label>
+
+          <input
+            checked={selectedOption === "basic"}
+            id="radio-basic"
+            name="radio"
+            type="radio"
+            onChange={() => setSelectedOption("basic")}
+          />
+          <label htmlFor="radio-basic">Basic</label>
+
+          <input
+            checked={selectedOption === "premium"}
+            id="radio-premium"
+            name="radio"
+            type="radio"
+            onChange={() => setSelectedOption("premium")}
+          />
+          <label htmlFor="radio-premium">Premium</label>
+
+          <div className={scss["glider-container"]}>
+            <div className={scss["glider"]}></div>
+          </div>
+        </div>
       </div>
     </div>
   );
