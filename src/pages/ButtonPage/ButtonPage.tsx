980b32d51609c00aa9a67eed4a82fdcfb3d1a09c
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2025-05-02 00:09:25 +0200

    02.05.2025

--- src/pages/ButtonPage/ButtonPage.tsx
+++ src/pages/ButtonPage/ButtonPage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaUser } from "react-icons/fa";
 import scss from "./ButtonPage.module.scss";
 
 export default function ButtonPage() {
@@ -67,6 +68,28 @@ export default function ButtonPage() {
           onClick={handleOnClick}>
           Button 4
         </button>
+        <div className={scss["button-5-container"]}>
+          <button className={scss["button-5"]}>
+            <span className={scss["text"]}>Button 5</span>
+            <span className={scss["icon"]}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24">
+                <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+              </svg>
+            </span>
+          </button>
+        </div>
+        <div className={scss["button-6-container"]}>
+          <button className={scss["button-6"]}>
+            <span className={scss["text"]}>Button 6</span>
+            <span className={scss["icon-container"]}>
+              <FaUser className={scss["icon"]} />
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   );
