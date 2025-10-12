38ba1349f2dc31e9930c3104d3e4b6886e724dc8
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2025-10-12 13:33:41 +0200

    Update IconsPage.tsx

--- src/pages/IconsPage/IconsPage.tsx
+++ src/pages/IconsPage/IconsPage.tsx
@@ -110,7 +110,7 @@ export default function IconsPage() {
       <article className={scss["article-icons-page-7"]}>
         <div>
           <p>
-            📊📉📈📅📆🗓️📅📄⛔⚠️🖨️🖥️💽💾💿📀📔📕📖📗📘📚📓📒📃📜📄📄📑📝📧📫🎫
+            📊📉📈📅📆🗓️📅📄⛔⚠️🖨️🖥️💽💾💿📀📔📕📖📗📘📚📓📒📃📜📄📄📑📝📧📫🎫0️⃣1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣7️⃣8️⃣9️⃣🔟
           </p>
         </div>
       </article>
